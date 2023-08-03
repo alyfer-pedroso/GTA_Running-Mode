b88abe91fcc7fb31bb123c9cd68bc0fa35af0bce
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2023-08-03 03:00:53 -0300

    edit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,74 @@
 (() => {
+    const game = document.querySelector(".game");
+    const scene = document.querySelector(".scene");
     const player = document.querySelector(".player");
-    const playerRun = document.querySelector(".manRun");
-    const pipe = document.querySelector(".pipe");
+    const playerAnim = document.querySelector(".playerAnim");
+    const audioPlay = document.querySelector(".audioPlay");
+    const soundTrack = document.querySelector(".soundTrack audio");
 
-    const jump = () => {
-        player.classList.add("jump");
-        setTimeout(() => {
-            player.classList.remove("jump");
-        }, 750);
+    let jump = false;
+
+    const jumping = (el) => {
+        if (el.key == " " || el.key == "w") {
+            playerAnim.style.setProperty("animation", "jump 0s steps(1) forwards");
+            game.removeEventListener("pointerdown", jumping);
+            document.removeEventListener("keypress", jumping);
+            jump = true;
+        } else {
+            playerAnim.style.setProperty("animation", "jump 0s steps(1) forwards");
+            game.removeEventListener("pointerdown", jumping);
+            document.removeEventListener("keypress", jumping);
+            jump = true;
+        }
     };
 
-    const loopInGame = setInterval(() => {
-        const pipePosition = pipe.offsetLeft;
-        const playerPosition = Number(window.getComputedStyle(player).bottom.replace("px", ""));
+    const gamePhysics = setInterval(() => {
+        let velocityY = 0;
+        let playerPositionY = Number(getComputedStyle(player).bottom.replace("px", ""));
+        let playerLastPositionY = playerPositionY;
 
-        // if (pipePosition <= 80 && pipePosition > 0 && playerPosition < 80) {
-        //     pipe.style.animation = "none";
-        //     pipe.style.left = `${pipePosition}px`;
+        if (playerPositionY > 40) {
+            velocityY -= 2;
+            playerLastPositionY += velocityY;
+            player.style.bottom = `${playerLastPositionY}px`;
+        } else {
+            velocityY = 0;
+            playerLastPositionY = 40;
+            player.style.bottom = `${playerLastPositionY}px`;
+        }
 
-        //     playerRun.style.animation = "none";
-        //     player.style.bottom = `${playerPosition}px`;
+        if (jump && playerPositionY <= 200) {
+            playerLastPositionY += 4;
+            player.style.bottom = `${playerLastPositionY}px`;
+        } else {
+            jump = false;
+            playerAnim.style.setProperty("animation", "jumpFall .2s steps(1) forwards");
+            if (playerLastPositionY == 40) {
+                playerAnim.style.setProperty("animation", "run 0.6s steps(8) infinite");
+                game.addEventListener("pointerdown", jumping);
+                document.addEventListener("keypress", jumping);
+            }
+        }
 
-        //     document.removeEventListener("keyup", jump);
-        //     document.removeEventListener("pointerdown", jump);
+        velocityY = 0;
+        velocityY -= 2;
+    });
 
-        //     clearInterval(loopInGame);
-        // }
-    }, 10);
+    soundTrack.pause();
+    audioPlay.addEventListener("click", () => {
+        if (soundTrack.classList.contains("paused")) {
+            soundTrack.play();
+            audioPlay.classList.remove("fa-volume-xmark");
+            audioPlay.classList.add("fa-volume-high");
+            soundTrack.classList.remove("paused");
+        } else {
+            soundTrack.pause();
+            audioPlay.classList.add("fa-volume-xmark");
+            audioPlay.classList.remove("fa-volume-high");
+            soundTrack.classList.add("paused");
+        }
+    });
 
-    document.addEventListener("keyup", jump);
-    document.addEventListener("pointerdown", jump);
+    game.addEventListener("pointerdown", jumping);
+    document.addEventListener("keypress", jumping);
 })();
